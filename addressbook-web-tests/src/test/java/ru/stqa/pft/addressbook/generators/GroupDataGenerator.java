fbdf9c8b7bf4c0ffa906839424e5c5504f1e2786
alexv767 <alexveli@mail.ru>
2018-11-26 20:45:50 +0300

    XML added.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,8 +3,8 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
-import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -21,6 +21,9 @@ public class GroupDataGenerator {
     @Parameter (names = "-f", description = "Target file")
     public  String file;
 
+    @Parameter (names = "-d", description = "Data format")
+    public  String format;
+
     public static void main(String[] args) throws IOException {
         GroupDataGenerator generator = new GroupDataGenerator();
         JCommander jCommander = new JCommander(generator);
@@ -35,10 +38,26 @@ public class GroupDataGenerator {
 
     private void run() throws IOException {
         List<GroupData> groups = generateGroups(count);  // generate number of groups
-        save(groups, new File(file));  // save generated data into file
+        if (format.equals("csv")){
+            saveAsCsv(groups, new File(file));  // saveAsCsv generated data into file
+        } else if (format.equals("xml")){
+            saveAsXml(groups, new File(file));  // saveAsCsv generated data into file
+        } else {
+            System.out.println("Unrecognized format" + format);
+        }
     }
 
-    private void save(List<GroupData> groups, File file) throws IOException {
+    private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+        //xstream.alias("group", GroupData.class);
+        xstream.processAnnotations(GroupData.class);
+        String xml = xstream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for(GroupData group : groups){
