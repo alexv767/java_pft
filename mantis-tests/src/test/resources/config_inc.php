b5bf30dfc44353d77582e5f45c7ed0d307b75b94
alexv767 <alexveli@mail.ru>
2018-12-08 11:48:34 +0300

    FTP Helper added (config file corrections).

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -1,7 +1,8 @@
 <?php
 $g_hostname = 'localhost';
-$g_db_type = 'mysql';
+$g_db_type = 'mysqli';
 $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
+$g_crypto_master_salt     = 'rPR8bh0TVxpj8kgvusaXDqOQyBMeYpnjv9VTZf9QF8k=';
 $g_signup_use_captcha = OFF;
