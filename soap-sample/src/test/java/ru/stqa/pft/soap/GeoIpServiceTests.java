af509289a5e2e3e2422fbca90a7361437a15edec
alexv767 <alexveli@mail.ru>
2018-12-09 21:04:55 +0300

    GEO IP service added (test not workable).

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp() {
+//        GeoIP geoIp = new GeoIPService().getGeoIPServiceSoap12().getGeoIP();
+        GeoIP geoIp = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("109.252.26.16");
+        assertEquals(geoIp.getCountryCode(), "RUS");
+    }
+}
