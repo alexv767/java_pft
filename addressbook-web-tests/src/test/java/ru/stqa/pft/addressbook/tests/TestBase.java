cf912b8144d42fd87b1027af2f7e6e62d340ab99
alexv767 <alexveli@mail.ru>
2018-12-15 12:13:04 +0300

    run with Jenkins (v04)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -23,8 +23,9 @@ public class TestBase {
 //    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     public static final ApplicationManager app =
-//            new ApplicationManager(BrowserType.FIREFOX);
-            new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+//    new ApplicationManager(BrowserType.FIREFOX);
+//    new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+    new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {
